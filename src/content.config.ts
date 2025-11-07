026335d22851564aec50d8722e8b0996d0814f40
Tim Willebrands <timwillebrands@gmail.com>
2025-11-07 20:36:53 +0100

    oopsie

--- src/content.config.ts
+++ src/content.config.ts
@@ -8,7 +8,7 @@ const blog = defineCollection({
 	schema: ({ image }) =>
 		z.object({
 			title: z.string(),
-			description: z.string(),
+			summary: z.string(),
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
